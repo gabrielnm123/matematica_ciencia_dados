db86fae9c109c00b1588cef17461db111f46f1ab
gabrielnm123 <gabrielnmatos96@gmail.com>
2025-03-30 17:13:11 -0300

    aula 3

--- Aula 3: Matrizes Especiais, Transposição, Inversa e Produto Escalar/exercicios.py
+++ Aula 3: Matrizes Especiais, Transposição, Inversa e Produto Escalar/exercicios.py
@@ -6,3 +6,29 @@ import numpy as np
 # crie dois vetores p e q (com a mesma quantidade de elementos) de sua escolha e calcule o produto escalar entre eles.
 # mostre os resultados na tela.
 
+# Matriz 3x3
+B = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+print(f'B:\n{B}\n')
+
+# Transposta de B
+B_transpose = B.transpose() # ou B.T
+print(f'B transposta:\n{B_transpose}\n')
+
+# Verifica se B é invertível
+try:
+    B_inv = np.linalg.inv(B)
+    print(f'Inversa de B:\n{B_inv}\n')
+except:
+    print('B não é invertível.\n')
+
+# Vetores p e q
+p = np.array([1, 2, 3])
+q = np.array([4, 5, 6])
+print(f'p:\n{p}\n')
+print(f'q:\n{q}\n')
+
+# Produto escalar entre p e q
+produto_escalar = np.dot(p, q) # ou p @ q
+print(f'Produto escalar entre p e q:\n{produto_escalar}\n')
+
+# explore mais funções de álgebra linear no numpy.linalg
